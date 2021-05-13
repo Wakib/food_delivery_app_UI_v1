864d4367bd77bdfde0888ac183a54ed9cd7e326c
Wakib <wakib55@gmail.com>
2021-05-13 21:43:43 +0600

    Home Screen added

---
+++ lib/constants.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+const kTextColor = Color(0xFF1D150B);
+const kTextCustomColor = Color(0xFF1D150B);
+const kTPrimaryColor = Color(0xFFB475F);
+const kSecondaryColor = Color(0xFFF5E1CB);
+const kBorderColor = Color(0xFFDDDDDD);
